a1eb1fef2237d514f63c6b150c1139f9076545ed
Thomas <thomas.j.william@gmail.com>
2021-03-03 17:24:39 +0000

    with style

--- src/router/index.ts
+++ src/router/index.ts
@@ -18,6 +18,15 @@ const routes: Array<RouteConfig> = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "todo" */ "../views/Todo.vue")
+  },
+  {
+    path: "/style",
+    name: "Style",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "style" */ "../views/Style.vue")
   }
 ];
 
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="todo">
     <h1>Todos</h1>
+
+    <ul>
+      <li>Eat with fingers</li>
+      <li>Walk Santiago de Compostela</li>
+    </ul>
+
   </div>
 </template>
+
+<style lang="scss">
+</style>
