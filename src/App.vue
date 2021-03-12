4632093daf89de0107d0977cbcecbdf21ce2603b
Thomas <thomas.j.william@gmail.com>
2021-03-12 10:45:32 +0000

    stuff

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,53 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  <div id="app" class="flex column gaps">
+    <div id="nav" class="box">
       <router-link to="/">Todos</router-link> |
-      <router-link to="/style">Style</router-link> |
-      <a href="https://github.com/thomasjwilliam/data/blob/main/todos.json">Source</a>
+      <router-link to="/style">Style</router-link>
     </div>
-    <router-view />
+    <main class="box grow">
+      <router-view class="" />
+    </main>
+    <div class="box">
+      <a href="https://github.com/thomasjwilliam/data/blob/main/todos.json"
+        >Github data source</a
+      > |
+      <a href="https://docs.google.com/spreadsheets/d/1hrgkcJVAvE6ApOX-F0s0tsw2hWUe8OQFeHcl52TgYBc/edit#gid=0"
+        >Sheets data source</a
+      >
+    </div>
   </div>
 </template>
 
 <style>
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    /*text-align: center;*/
-    color: #2c3e50;
-  }
+html,
+body {
+  height: 100%;
+}
 
-  #nav {
-    /*padding: 30px;*/
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  height: 100%;
+}
 
-  #nav a {
-    font-weight: bold;
-    color: #2c3e50;
-  }
+#nav {
+  /*padding: 30px;*/
+}
 
-  #nav a.router-link-exact-active {
-    color: #42b983;
-  }
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+main {
+  /*display: flex;*/
+  /*flex-flow: row wrap;*/
+  /*align-items: flex-start;*/
+}
 </style>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -3,8 +3,8 @@
     <h1>Todos</h1>
 
     <div v-for="todo in todos" :key="todo.id">
-      <h3 v-if="todo.url">
-        <a :href="todo.url">{{ todo.title }}</a>
+      <h3 v-if="todo.source">
+        <a :href="todo.source">{{ todo.title }}</a>
       </h3>
       <h3 v-else>{{ todo.title }}</h3>
     </div>
@@ -17,14 +17,35 @@ import todos from "@/views/todos.json";
 
 @Component
 export default class Todo extends Vue {
-  todos = null;
+  todos: Array<any> = [];
   todosFake = todos;
 
   mounted() {
-    fetch("https://thomasjwilliam.github.io/data/todos.json")
+    // fetch("https://thomasjwilliam.github.io/data/todos.json")
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     this.todos = data;
+    //   });
+
+    const url = "https://spreadsheets.google.com/feeds/list/1hrgkcJVAvE6ApOX-F0s0tsw2hWUe8OQFeHcl52TgYBc/od6/public/values?alt=json";
+    const url2 = "https://spreadsheets.google.com/feeds/list/2PACX-1vSMa3Z8-ytmYHW_5qsaqFmI28660FkYDGZZEdi974JvMvFsn-vccS3fiJ8vI8P6MFsRZCW2GlvG8BPS/od6/public/values?alt=json";
+    // 2PACX-1vSMa3Z8-ytmYHW_5qsaqFmI28660FkYDGZZEdi974JvMvFsn-vccS3fiJ8vI8P6MFsRZCW2GlvG8BPS
+
+    fetch(url)
       .then(response => response.json())
       .then(data => {
-        this.todos = data;
+        const output = [];
+        const entries = data.feed.entry;
+        // console.log(entries);
+        for (const entry of entries) {
+          output.push({
+            id: entry.gsx$id.$t,
+            title: entry.gsx$title.$t,
+            source: entry.gsx$source.$t
+          });
+        }
+        // console.log(output);
+        this.todos = output;
       });
   }
 }
